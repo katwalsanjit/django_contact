f4b5eebdebafeda75692b58c67692248fd726ba2
sanjit69 <sanjitkatwal@gmail.com>
2019-07-16 12:51:38 +0545

    solved the simple type of error

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import Contact
+from import_export.admin import ImportExportModelAdmin
 
 
-class ContactAdmin(admin.ModelAdmin):
+class ContactAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'gender', 'email', 'info', 'phone')
     list_display_links = ('id', 'name')
     list_editable = ('info',)
